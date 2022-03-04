43901900d13bbf80053f8b6b358f326655f799d9
Miglius Mockus <74502967+migliusmockus@users.noreply.github.com>
2022-03-04 20:31:44 +0100

    Update main.js
    added extra code which will connect and save data to local storage so that when you refresh the website, your details remain

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-let myLeads = []
+let myDetails = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+
+let detailsFromLocalStorage = JSON.parse(localStorage.getItem("myDetails"))
+if(detailsFromLocalStorage) {
+    myDetails = detailsFromLocalStorage
+    renderDetails()
+}
+else {
+    ulEl.innerHTML = "Nothing currently saved"
+}
 
 inputBtn.addEventListener("click", function() {
-    console.log("button clicked on add event listener")
+    myDetails.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myDetails", JSON.stringify(myDetails))
+    renderDetails()
 })
+
+function renderDetails() {
+    let listItems = ""
+    for (let i = 0; i < myDetails.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myDetails[i]}'>
+                    ${myDetails[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
