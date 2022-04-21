6ca0aebf1d1e75bdf7a242a1ab79f64c7f86d2f4
Miglius Mockus <74502967+migliusmockus@users.noreply.github.com>
2022-04-21 20:48:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let myDetails = [] // an empty array in which we will store saved data a user en
 const inputEl = document.getElementById("input-el") // creating a variable which gets the id of the input field in the HTML file
 const inputBtn = document.getElementById("input-btn") // creating a variable which gets the id of the button in the HTML file
 const ulEl = document.getElementById("ul-el") // creating a variable which gets the id of unordered list in the HTML file
-const deleteBtn = document.getElementById("delete-btn")
-const saveTabBtn = document.getElementById("save-tab-btn")
+const deleteBtn = document.getElementById("delete-btn") // creating a variable which will be used to delete everything saved in the extension
+const deletelastBtn = document.getElementById("deletelast-btn") // creating a variable which will delete the last item saved in the extension
 
 const detailsFromLocalStorage = JSON.parse(localStorage.getItem("myDetails")) // here i created a variable which will first parse/change our saved tasks/details which are strings because we used stringify() to array objects and retrieve them from local storage so that we could display the tasks
 if(detailsFromLocalStorage) { // an if statement which checks if detailsFromLocalStorage variable has any saved data in myDetails array
     myDetails = detailsFromLocalStorage // if the statement is true, myDetails array is then equal to the detailsFromLocalStorage variable and gets all the saved tasks/data user entered
-    renderDetails(myDetails) // renderDetails() will run a function called renderDetails 
+    renderTab(myDetails) // renderTab() will run a function called renderDetails 
 }
 else {
     ulEl.innerHTML = "Nothing currently saved" // will just display on the screen for the user that no data is currently saved in the extension
@@ -19,7 +19,7 @@ saveTabBtn.addEventListener("click", function() { // when a user clicks on the s
         console.log(tabs) // just logging out to test if everything works
         myDetails.push(tabs[0].url) // this is where we store everything saved as an url in the extension
         localStorage.setItem("myDetails", JSON.stringify(myDetails)) // here we just say that local storage will set our items that are in the myDetails array to make them into strings from objects.
-        renderDetails(myDetails) // here we run the renderDetails function and pass in our array as a parameter which the function is going to use to run properly
+        renderTab(myDetails) // here we run the renderTab function and pass in our array as a parameter which the function is going to use to run properly
     })
 })
 
@@ -52,7 +52,7 @@ deleteLastBtn.addEventListener("click", function() {
     renderTab(myDetails) // will run a function called myDetails
     if (detailsFromLocalStorage) { // an if statement which checks if detailsFromLocalStorage variable has any saved data in myDetails array
         myDetails = detailsFromLocalStorage // if the statement is true, myDetails array is then equal to the detailsFromLocalStorage variable and gets all the saved tasks/data user entered
-        renderTab(myDetails) // renderDetails() will run a function called renderTab
+        renderTab(myDetails) // renderTab() will run a function called renderTab
     } else {
         ulEl.innerHTML = "Nothing currently saved" // will just display on the screen for the user that no data is currently saved in the extension
     }
@@ -62,7 +62,7 @@ inputBtn.addEventListener("click", function() { // an event listener which is go
     myDetails.push(inputEl.value) // will get the item from input list and put it in the empty array that we created, later as we add more items/tasks, the push() function will place our tasks at the end of the array
     inputEl.value = "" // this will reset the input field to be empty so that user doesn't need to erase the field himself when adding another task
     localStorage.setItem("myDetails", JSON.stringify(myDetails)) // here we just say that local storage will set our items that are in the myDetails array to make them into strings from objects.
-    renderDetails(myDetails) // renderDetails() will run another function called renderDetails  
+    renderTab(myDetails) // renderTab() will run another function called renderDetails  
 })
 
 
